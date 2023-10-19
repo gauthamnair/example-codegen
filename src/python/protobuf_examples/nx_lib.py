10276142a142e11b1c689776e5c5269a6db91f80
Gautham Nair <gautham.p.nair@gmail.com>
2023-10-19 09:50:38 -0400

    two resolves working

---
+++ src/python/protobuf_examples/nx_lib.py
@@ -0,0 +1,5 @@
+import networkx
+
+
+def version_string():
+    return f'Nx version: {networkx.__version__}'
---
+++ src/python/protobuf_examples/nx_script.py
@@ -0,0 +1,5 @@
+from .nx_lib import version_string
+
+
+if __name__ == "__main__":
+    print(version_string())
